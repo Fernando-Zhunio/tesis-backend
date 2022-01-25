0dae2bb630881f420ac3fc69f919e17aea2c79af
fernando_alfonso_zhunio <fzhunio91@hotmail.com>
2022-01-24 20:37:45 -0500

    add git push

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,9 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
-
+                        <li class="nav-item">
+                            <a href="" class="nav-link">Eventos</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
